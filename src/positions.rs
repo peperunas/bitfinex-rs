66cfcf0bddaa43c35dae1118e8da32a0cb25b090
Giulio De Pasquale <depasquale@giugl.io>
2021-01-03 15:23:08 +0000

    removed position meta structure

--- src/positions.rs
+++ src/positions.rs
@@ -4,6 +4,7 @@ use serde_json::from_str;
 
 use crate::client::Client;
 use crate::errors::BoxError;
+use std::collections::HashMap;
 
 fn coercible<'de, D, T>(deserializer: D) -> Result<T, D::Error> where
     T: DeserializeOwned,
@@ -13,18 +14,6 @@ fn coercible<'de, D, T>(deserializer: D) -> Result<T, D::Error> where
     let coercible_string = String::deserialize(deserializer)?;
 
     from_str(&coercible_string).map_err(Error::custom)
-}
-
-#[derive(Serialize, Deserialize, Debug, Clone)]
-pub struct PositionMeta {
-    reason: String,
-    order_id: u64,
-    order_id_oppo: u64,
-    liq_stage: Option<String>,
-    #[serde(deserialize_with = "coercible")]
-    trade_price: f64,
-    #[serde(deserialize_with = "coercible")]
-    trade_amount: f64,
 }
 
 #[derive(Serialize, Deserialize, Debug, Clone)]
@@ -51,7 +40,61 @@ pub struct Position {
     _placeholder3: Option<String>,
     collateral: f64,
     collateral_min: f64,
-    meta: Option<PositionMeta>,
+    meta: Option<HashMap<String, serde_json::Value>>,
+}
+
+impl Position {
+    pub fn symbol(&self) -> &str {
+        &self.symbol
+    }
+    pub fn status(&self) -> &str {
+        &self.status
+    }
+    pub fn amount(&self) -> f64 {
+        self.amount
+    }
+    pub fn base_price(&self) -> f64 {
+        self.base_price
+    }
+    pub fn margin_funding(&self) -> f64 {
+        self.margin_funding
+    }
+    pub fn margin_funding_type(&self) -> u8 {
+        self.margin_funding_type
+    }
+    pub fn pl(&self) -> f64 {
+        self.pl
+    }
+    pub fn pl_perc(&self) -> f64 {
+        self.pl_perc
+    }
+    pub fn price_liq(&self) -> f64 {
+        self.price_liq
+    }
+    pub fn leverage(&self) -> f64 {
+        self.leverage
+    }
+    pub fn position_id(&self) -> u64 {
+        self.position_id
+    }
+    pub fn mts_create(&self) -> Option<u64> {
+        self.mts_create
+    }
+    pub fn mts_update(&self) -> Option<u64> {
+        self.mts_update
+    }
+    pub fn position_type(&self) -> u64 {
+        self.position_type
+    }
+    pub fn collateral(&self) -> f64 {
+        self.collateral
+    }
+    pub fn collateral_min(&self) -> f64 {
+        self.collateral_min
+    }
+    pub fn meta(&self) -> &Option<HashMap<String, serde_json::Value>> {
+        &self.meta
+    }
 }
 
 #[derive(Clone)]
