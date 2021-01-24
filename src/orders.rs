a6e54e3008e51630f7555b0a71072c6c61b86fb4
Giulio De Pasquale <giulio@depasquale.eu>
2021-01-24 13:15:51 +0000

    Changed fields of ActiveOrder. Implemented Deserialize for ActiveOrder. Stabilized field names between ActiveOrder and OrderResponse

--- src/orders.rs
+++ src/orders.rs
@@ -9,47 +9,335 @@ use crate::client::Client;
 use crate::endpoints::AuthenticatedEndpoint;
 use crate::errors::BoxError;
 
-#[derive(Serialize, Deserialize, Clone)]
+#[derive(Serialize, Clone, Debug)]
 pub struct ActiveOrder {
-    pub id: u64,
-    pub group_id: Option<u32>,
-    pub client_id: u64,
-    pub symbol: String,
-    pub creation_timestamp: u64,
-    pub update_timestamp: u64,
-    pub amount: f64,
-    pub amount_original: f64,
-    pub order_type: String,
-    pub previous_order_type: Option<String>,
+    /// Order ID
+    id: u64,
+    /// Group ID
+    group_id: Option<u64>,
+    /// Client Order ID
+    client_id: u64,
+    /// Pair (tBTCUSD, …)
+    symbol: String,
+    /// Millisecond timestamp of creation
+    creation_timestamp: u64,
+    /// Millisecond timestamp of update
+    update_timestamp: u64,
+    /// Positive means buy, negative means sell.
+    amount: f64,
+    /// Original amount
+    amount_original: f64,
+    /// The type of the order: LIMIT, EXCHANGE LIMIT, MARKET, EXCHANGE MARKET, STOP, EXCHANGE STOP,
+    /// STOP LIMIT, EXCHANGE STOP LIMIT, TRAILING STOP, EXCHANGE TRAILING STOP, FOK, EXCHANGE FOK,
+    /// IOC, EXCHANGE IOC.
+    order_type: OrderKind,
+    /// Previous order type
+    previous_order_type: Option<OrderKind>,
+    /// Active flags for order
+    flags: OrderFlags,
+    /// Order Status: ACTIVE, PARTIALLY FILLED, RSN_PAUSE (trading is paused / paused due to AMPL rebase event)
+    order_status: String,
+    /// Price
+    price: f64,
+    /// Average price
+    price_avg: Option<f64>,
+    /// The trailing price
+    price_trailing: Option<f64>,
+    /// Auxiliary Limit price (for STOP LIMIT)
+    price_aux_limit: Option<f64>,
+    /// Set if order is hidden on the order book
+    hidden: bool,
+    /// If another order caused this order to be placed (OCO) this will be that other order's ID
+    placed_id: Option<u64>,
+    /// indicates origin of action: BFX, API>BFX
+    routing: Option<String>,
+    /// Additional meta information about the order ( $F7 = IS_POST_ONLY (0 if false, 1 if true),
+    /// $F33 = Leverage (int), aff_code: "aff_code_here")
+    meta: Option<String>,
+}
 
-    #[serde(skip_serializing)]
-    _placeholder_1: Option<String>,
-    #[serde(skip_serializing)]
-    _placeholder_2: Option<String>,
+impl ActiveOrder {
+    pub fn id(&self) -> u64 {
+        self.id
+    }
+    pub fn group_id(&self) -> Option<u64> {
+        self.group_id
+    }
+    pub fn client_id(&self) -> u64 {
+        self.client_id
+    }
+    pub fn symbol(&self) -> &str {
+        &self.symbol
+    }
+    pub fn creation_timestamp(&self) -> u64 {
+        self.creation_timestamp
+    }
+    pub fn update_timestamp(&self) -> u64 {
+        self.update_timestamp
+    }
+    pub fn amount(&self) -> f64 {
+        self.amount
+    }
+    pub fn amount_original(&self) -> f64 {
+        self.amount_original
+    }
+    pub fn order_type(&self) -> OrderKind {
+        self.order_type
+    }
+    pub fn previous_order_type(&self) -> Option<OrderKind> {
+        self.previous_order_type
+    }
+    pub fn flags(&self) -> OrderFlags {
+        self.flags
+    }
+    pub fn order_status(&self) -> &str {
+        &self.order_status
+    }
+    pub fn price(&self) -> f64 {
+        self.price
+    }
+    pub fn price_avg(&self) -> Option<f64> {
+        self.price_avg
+    }
+    pub fn price_trailing(&self) -> Option<f64> {
+        self.price_trailing
+    }
+    pub fn price_aux_limit(&self) -> Option<f64> {
+        self.price_aux_limit
+    }
+    pub fn hidden(&self) -> bool {
+        self.hidden
+    }
+    pub fn placed_id(&self) -> Option<u64> {
+        self.placed_id
+    }
+    pub fn routing(&self) -> &Option<String> {
+        &self.routing
+    }
+    pub fn meta(&self) -> &Option<String> {
+        &self.meta
+    }
+}
 
-    pub flags: Option<u32>,
-    pub order_status: Option<String>,
+impl<'de> Deserialize<'de> for ActiveOrder {
+    fn deserialize<D>(deserializer: D) -> Result<Self, <D as Deserializer<'de>>::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let value = Value::deserialize(deserializer)?;
+        let mut iterator = value
+            .as_array()
+            .ok_or(D::Error::custom("Missing main array"))?
+            .iter();
 
-    #[serde(skip_serializing)]
-    _placeholder_3: Option<String>,
-    #[serde(skip_serializing)]
-    _placeholder_4: Option<String>,
+        let id = iterator
+            .next()
+            .ok_or(D::Error::custom("Missing order id"))?
+            .as_u64()
+            .ok_or(D::Error::custom("Invalid order id type"))?;
 
-    pub price: f64,
-    pub price_avg: f64,
-    pub price_trailing: Option<f64>,
-    pub price_aux_limit: Option<f64>,
+        let gid = Option::deserialize(
+            iterator
+                .next()
+                .ok_or(D::Error::custom("Missing order id"))?,
+        )
+        .map_err(D::Error::custom)?;
 
-    #[serde(skip_serializing)]
-    __placeholder_5: Option<String>,
-    #[serde(skip_serializing)]
-    _placeholder_6: Option<String>,
-    #[serde(skip_serializing)]
-    _placeholder_7: Option<String>,
+        let cid = iterator
+            .next()
+            .ok_or(D::Error::custom("Missing client id"))?
+            .as_u64()
+            .ok_or(D::Error::custom("Invalid client id type"))?;
 
-    pub notify: u32,
-    pub hidden: u32,
-    pub placed_id: Option<i32>,
+        let symbol =
+            String::deserialize(iterator.next().ok_or(D::Error::custom("Missing symbol"))?)
+                .map_err(D::Error::custom)?;
+
+        let mts_create = iterator
+            .next()
+            .ok_or(D::Error::custom("Missing mts create"))?
+            .as_u64()
+            .ok_or(D::Error::custom("Invalid mts create type"))?;
+
+        let mts_update = iterator
+            .next()
+            .ok_or(D::Error::custom("Missing mts update"))?
+            .as_u64()
+            .ok_or(D::Error::custom("Invalid mts update type"))?;
+
+        let amount = iterator
+            .next()
+            .ok_or(D::Error::custom("Missing amount"))?
+            .as_f64()
+            .ok_or(D::Error::custom("Invalid amount type"))?;
+
+        let amount_orig = iterator
+            .next()
+            .ok_or(D::Error::custom("Missing amount orig"))?
+            .as_f64()
+            .ok_or(D::Error::custom("Invalid amount orig type"))?;
+
+        let order_type = OrderKind::deserialize(
+            iterator
+                .next()
+                .ok_or(D::Error::custom("Missing order type"))?,
+        )
+        .map_err(D::Error::custom)?;
+
+        let prev_order_type = Option::deserialize(
+            iterator
+                .next()
+                .ok_or(D::Error::custom("Missing prev order type"))?,
+        )
+        .map_err(D::Error::custom)?;
+
+        // skip placeholders
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+
+        let flags = OrderFlags::from_bits(
+            iterator
+                .next()
+                .ok_or(D::Error::custom("Missing order flags"))?
+                .as_u64()
+                .ok_or(D::Error::custom("Invalid order flags type"))? as u32,
+        )
+        .ok_or(D::Error::custom("Invalid order flags"))?;
+
+        let order_status = String::deserialize(
+            iterator
+                .next()
+                .ok_or(D::Error::custom("Missing order type"))?,
+        )
+        .map_err(D::Error::custom)?;
+
+        // skip placeholders
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+
+        let price = iterator
+            .next()
+            .ok_or(D::Error::custom("Missing price"))?
+            .as_f64()
+            .ok_or(D::Error::custom("Invalid price type"))?;
+
+        let price_avg = {
+            let value = iterator
+                .next()
+                .ok_or(D::Error::custom("Missing price average"))?
+                .as_f64()
+                .ok_or(D::Error::custom("Invalid price average type"))?;
+
+            if value > 0.0 {
+                Some(value)
+            } else {
+                None
+            }
+        };
+
+        let price_trailing = {
+            let value = iterator
+                .next()
+                .ok_or(D::Error::custom("Missing trailing price"))?
+                .as_f64()
+                .ok_or(D::Error::custom("Invalid trailing price"))?;
+
+            if value > 0.0 {
+                Some(value)
+            } else {
+                None
+            }
+        };
+
+        let price_aux_limit = {
+            let value = iterator
+                .next()
+                .ok_or(D::Error::custom("Missing price aux limit"))?
+                .as_f64()
+                .ok_or(D::Error::custom("Invalid price aux limit"))?;
+
+            if value > 0.0 {
+                Some(value)
+            } else {
+                None
+            }
+        };
+
+        // skip placeholders
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+
+        let hidden = iterator
+            .next()
+            .ok_or(D::Error::custom("Missing hidden"))?
+            .as_u64()
+            .ok_or(D::Error::custom("Invalid hidden type"))?
+            > 0;
+
+        let placed_id = Option::deserialize(
+            iterator
+                .next()
+                .ok_or(D::Error::custom("Missing placed_id"))?,
+        )
+        .map_err(D::Error::custom)?;
+
+        let routing =
+            Option::deserialize(iterator.next().ok_or(D::Error::custom("Missing routing"))?)
+                .map_err(D::Error::custom)?;
+
+        // skip placeholders
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+        iterator
+            .next()
+            .ok_or(D::Error::custom("Missing placeholder"))?;
+
+        // TODO: define specialized struct
+        let meta = Option::deserialize(iterator.next().ok_or(D::Error::custom("Missing meta"))?)
+            .map_err(D::Error::custom)?;
+
+        Ok(Self {
+            id,
+            group_id: gid,
+            client_id: cid,
+            symbol,
+            creation_timestamp: mts_create,
+            update_timestamp: mts_update,
+            amount,
+            amount_original: amount_orig,
+            order_type,
+            previous_order_type: prev_order_type,
+            flags,
+            order_status,
+            price,
+            price_avg,
+            price_trailing,
+            price_aux_limit,
+            hidden,
+            placed_id,
+            routing,
+            meta,
+        })
+    }
 }
 
 #[derive(Clone, Debug, Serialize)]
@@ -64,25 +352,25 @@ pub struct OrderResponse {
     /// Order ID
     id: u64,
     /// Group ID
-    gid: Option<u64>,
+    group_id: Option<u64>,
     /// Client Order ID
-    cid: u64,
+    client_id: u64,
     /// Pair (tBTCUSD, …)
     symbol: String,
     /// Millisecond timestamp of creation
-    mts_create: u64,
+    creation_timestamp: u64,
     /// Millisecond timestamp of update
-    mts_update: u64,
+    update_timestamp: u64,
     /// Positive means buy, negative means sell.
     amount: f64,
     /// Original amount
-    amount_orig: f64,
+    amount_original: f64,
     /// The type of the order: LIMIT, EXCHANGE LIMIT, MARKET, EXCHANGE MARKET, STOP, EXCHANGE STOP,
     /// STOP LIMIT, EXCHANGE STOP LIMIT, TRAILING STOP, EXCHANGE TRAILING STOP,
     /// FOK, EXCHANGE FOK, IOC, EXCHANGE IOC.
     order_type: OrderKind,
     /// Previous order type
-    prev_order_type: Option<OrderKind>,
+    previous_order_type: Option<OrderKind>,
     /// Millisecond timestamp of Time-In-Force: automatic order cancellation
     mts_tif: Option<u64>,
     /// Order Status: ACTIVE, EXECUTED @ PRICE(AMOUNT) e.g. "EXECUTED @ 107.6(-0.2)",
@@ -94,11 +382,11 @@ pub struct OrderResponse {
     /// Price
     price: f64,
     /// Average price
-    price_avg: f64,
+    price_avg: Option<f64>,
     /// The trailing price
-    price_trailing: f64,
+    price_trailing: Option<f64>,
     /// Auxiliary Limit price (for STOP LIMIT)
-    price_aux_limit: f64,
+    price_aux_limit: Option<f64>,
     /// Flag is order is hidden
     hidden: bool,
     /// If another order caused this order to be placed (OCO) this will be that other order's ID
@@ -131,31 +419,31 @@ impl OrderResponse {
         self.id
     }
     pub fn gid(&self) -> Option<u64> {
-        self.gid
+        self.group_id
     }
     pub fn cid(&self) -> u64 {
-        self.cid
+        self.client_id
     }
     pub fn symbol(&self) -> &str {
         &self.symbol
     }
     pub fn mts_create(&self) -> u64 {
-        self.mts_create
+        self.creation_timestamp
     }
     pub fn mts_update(&self) -> u64 {
-        self.mts_update
+        self.update_timestamp
     }
     pub fn amount(&self) -> f64 {
         self.amount
     }
     pub fn amount_orig(&self) -> f64 {
-        self.amount_orig
+        self.amount_original
     }
     pub fn order_type(&self) -> OrderKind {
         self.order_type
     }
     pub fn prev_order_type(&self) -> Option<OrderKind> {
-        self.prev_order_type
+        self.previous_order_type
     }
     pub fn mts_tif(&self) -> Option<u64> {
         self.mts_tif
@@ -166,13 +454,13 @@ impl OrderResponse {
     pub fn price(&self) -> f64 {
         self.price
     }
-    pub fn price_avg(&self) -> f64 {
+    pub fn price_avg(&self) -> Option<f64> {
         self.price_avg
     }
-    pub fn price_trailing(&self) -> f64 {
+    pub fn price_trailing(&self) -> Option<f64> {
         self.price_trailing
     }
-    pub fn price_aux_limit(&self) -> f64 {
+    pub fn price_aux_limit(&self) -> Option<f64> {
         self.price_aux_limit
     }
     pub fn hidden(&self) -> bool {
@@ -354,23 +642,47 @@ impl<'de> Deserialize<'de> for OrderResponse {
             .as_f64()
             .ok_or(D::Error::custom("Invalid price type"))?;
 
-        let price_avg = middle_list_iter
-            .next()
-            .ok_or(D::Error::custom("Missing price average"))?
-            .as_f64()
-            .ok_or(D::Error::custom("Invalid price average type"))?;
+        let price_avg = {
+            let value = middle_list_iter
+                .next()
+                .ok_or(D::Error::custom("Missing price average"))?
+                .as_f64()
+                .ok_or(D::Error::custom("Invalid price average type"))?;
 
-        let price_trailing = middle_list_iter
-            .next()
-            .ok_or(D::Error::custom("Missing price trailing "))?
-            .as_f64()
-            .ok_or(D::Error::custom("Invalid price trailing type"))?;
+            if value > 0.0 {
+                Some(value)
+            } else {
+                None
+            }
+        };
 
-        let price_aux_limit = middle_list_iter
-            .next()
-            .ok_or(D::Error::custom("Missing price aux limit"))?
-            .as_f64()
-            .ok_or(D::Error::custom("Invalid price aux limit type"))?;
+        let price_trailing = {
+            let value = middle_list_iter
+                .next()
+                .ok_or(D::Error::custom("Missing trailing price"))?
+                .as_f64()
+                .ok_or(D::Error::custom("Invalid trailing price"))?;
+
+            if value > 0.0 {
+                Some(value)
+            } else {
+                None
+            }
+        };
+
+        let price_aux_limit = {
+            let value = middle_list_iter
+                .next()
+                .ok_or(D::Error::custom("Missing price aux limit"))?
+                .as_f64()
+                .ok_or(D::Error::custom("Invalid price aux limit"))?;
+
+            if value > 0.0 {
+                Some(value)
+            } else {
+                None
+            }
+        };
 
         // skip placeholders
         middle_list_iter
@@ -439,15 +751,15 @@ impl<'de> Deserialize<'de> for OrderResponse {
             response_type,
             message_id,
             id,
-            gid,
-            cid,
+            group_id: gid,
+            client_id: cid,
             symbol,
-            mts_create,
-            mts_update,
+            creation_timestamp: mts_create,
+            update_timestamp: mts_update,
             amount,
-            amount_orig,
+            amount_original: amount_orig,
             order_type,
-            prev_order_type,
+            previous_order_type: prev_order_type,
             mts_tif,
             order_status,
             price,
